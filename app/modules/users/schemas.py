daa7c83d61583d9583e1e37e8bb7282cf1e9e2c6
mayldute <mayldute@icloud.com>
2026-09-23 13:57:51 +0200

    update user's schemas

--- app/modules/users/schemas.py
+++ app/modules/users/schemas.py
@@ -1,6 +1,6 @@
 import uuid
 
-from pydantic import BaseModel, ConfigDict, EmailStr, Field
+from pydantic import BaseModel, ConfigDict, EmailStr, Field, model_validator
 
 
 class UserCreate(BaseModel):
@@ -8,6 +8,56 @@ class UserCreate(BaseModel):
     email: EmailStr = Field(
         ..., description="User's email.", examples=["user123@example.com"]
     )
+    password1: str = Field(
+        ..., 
+        min_length=8, 
+        description="User's password.", 
+        examples=["StrongPass123!"],
+    )
+    password2: str = Field(
+        ...,
+        min_length=8,
+        description="Password confirmation.",
+        examples=["StrongPass123!"],
+    )
+
+    @model_validator(mode="after")
+    def check_passwords(self):
+        if not any(c.islower() for c in self.password1):
+            raise ValueError("Password must contain at least one lowercase letter.")
+
+        if not any(c.isupper() for c in self.password1):
+            raise ValueError("Password must contain at least one uppercase letter.")
+
+        if not any(c.isdigit() for c in self.password1):
+            raise ValueError("Password must contain at least one digit.")
+
+        if self.password1 != self.password2:
+            raise ValueError("Passwords do not match.")
+
+        return self
+
+
+class UserLogin(BaseModel):
+    nickname: str | None = Field(
+        default=None, description="User's nickname.", examples=["user123"]
+    )
+    email: EmailStr | None = Field(
+        default=None, description="User's email.", examples=["user123@example.com"]
+    )
+    password: str = Field(
+        ..., description="Password confirmation.", examples=["StrongPass123!"]
+    )
+
+    @model_validator(mode="after")
+    def nickname_or_email(self):
+        if self.nickname is None and self.email is None:
+            raise ValueError("Either nickname or email must be provided.")
+
+        if self.nickname is not None and self.email is not None:
+            raise ValueError("Provide either nickname or email, not both.")
+
+        return self
 
 
 class UserResponse(BaseModel):
@@ -22,3 +72,13 @@ class UserResponse(BaseModel):
     )
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class Token(BaseModel):
+    access_token: str = Field(
+        ..., description="JWT access token", examples=["eyJhbGciOiJIUzI1..."]
+    )
+    refresh_token: str = Field(
+        ..., description="JWT refresh token", examples=["eyJhbGciOiJIUzI1..."]
+    )
+    token_type: str = Field(..., description="Token type", example="bearer")
